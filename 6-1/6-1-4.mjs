c54026f537343177d79236c0e139e43c06671253
h-fuzita <nkhshc421036@g.neec.ac.jp>
2024-12-23 11:45:37 +0900

    6-1-4

---
+++ 6-1/6-1-4.mjs
@@ -0,0 +1,20 @@
+const  a1 = false
+const  b1 = false
+console.log(a1 && b1)
+console.log(a1 || b1)
+console.log(! a1)
+console.log(! b1)
+
+const  a2 = true
+const  b2 = true
+console.log(a2 && b2)
+console.log(a2 || b2)
+console.log(! a2)
+console.log(! b2)
+
+const  a3 = true
+const  b3 = false
+console.log(a3 && b3)
+console.log(a3 || b3)
+console.log(! a3)
+console.log(! b3)
