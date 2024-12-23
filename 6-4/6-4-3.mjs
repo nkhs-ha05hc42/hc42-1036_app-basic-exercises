e463482816eab817ef05d3c911481f50698464e8
h-fuzita <nkhshc421036@g.neec.ac.jp>
2024-12-23 14:18:16 +0900

    6-4-3

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,10 @@
+const A =50
+if(A >= 90){
+    console.log('Excellent')
+}else if(A >= 80){
+    console.log('Excellent')
+}else if(A >= 60){
+    console.log('Normal')
+}else{
+    console.log('Red')
+}
