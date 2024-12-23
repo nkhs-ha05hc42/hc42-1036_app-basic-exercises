21fa38a04d1a9eabeab5ebfa7351350e93165ba6
h-fuzita <nkhshc421036@g.neec.ac.jp>
2024-12-23 15:11:28 +0900

    6-6-2

---
+++ 6-6/6-6-2.mjs
@@ -0,0 +1,10 @@
+const map1 = new Map()
+map1.set("A", "カレー")
+map1.set("B", "定食")
+for (const [key, value] of map1) {
+    console.log(key, value)
+    }
+map1.delete("A")
+for (const [key, value] of map1) {
+    console.log(key, value)
+    }
